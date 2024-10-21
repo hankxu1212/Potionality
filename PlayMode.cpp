44df9ba9a6ab25f33299a714ba61d7f4150f419f
flowerflora <preciousmonkey2004@gmail.com>
2024-10-21 15:39:37 -0400

    removed clearing bakcground

--- PlayMode.cpp
+++ PlayMode.cpp
@@ -76,16 +76,7 @@ void PlayMode::draw(glm::uvec2 const &drawable_size)
 
 	UpdateStage(Module::UpdateStage::Render);
 	
-	//{
-	//	// ok why isn't the background clear...?
-	//	glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
-	//	glClearDepth(1.0f); //1.0 is actually the default value to clear the depth buffer to, but FYI you can change it.
-	//	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-	//	glEnable(GL_DEPTH_TEST);
-	//	glDepthFunc(GL_LESS); //this is the default depth comparison function, but FYI you can change it.
-	//	RenderText("abcdefghijklmnopqrstuvwxyz wow testing character's", -.90f,-0.8f, .003f, characters,font_texs);
-	//}
+	RenderText("abcdefghijklmnopqrstuvwxyz wow testing character's", -.90f,-0.8f, .003f, characters,font_texs);
 }
 
 void PlayMode::PushLayer(Layer* layer)
