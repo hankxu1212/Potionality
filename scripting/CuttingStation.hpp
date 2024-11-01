3fa2ff44e6083159d30f4a3e364b024def28e48d
zixinq <107233955+zixinq@users.noreply.github.com>
2024-11-01 14:16:03 -0400

    cutting station

---
+++ scripting/CuttingStation.hpp
@@ -0,0 +1,13 @@
+//
+//  CuttingStation.hpp
+//  
+//
+//  Created by Zixin Qiu on 11/1/24.
+//
+
+#ifndef CuttingStation_hpp
+#define CuttingStation_hpp
+
+#include <stdio.h>
+
+#endif /* CuttingStation_hpp */
