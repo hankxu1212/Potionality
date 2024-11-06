9ca8ff2bb08d81c1db10a370e0738c31ee9249bd
Jesse Liu <jliu644@gmail.com>
2024-11-06 17:54:35 -0500

    Added red potion recipe

--- scripting/WorkStation.cpp
+++ scripting/WorkStation.cpp
@@ -74,7 +74,15 @@ void WorkStation::Interact(InteractPayload* payload)
                     Player::Instance->brew();
                     // TODO: Refactor with recipes
                     // Current version is hard coded for testing purposes
-                    if (ingredientCounts.count("FlowerIngredient") == 1 && ingredientCounts["FlowerIngredient"] == 1) {
+                    if (ingredientCounts.count("MushroomCutIngredient") && ingredientCounts.count("FlowerIngredient")) {
+                        ingredientCounts.clear();
+                        isEmpty = true;
+                        glm::vec2 newPos(GetTransform()->position().x, GetTransform()->position().y - 64);
+                        Entity* newPotion = SceneManager::Get()->getScene()->Instantiate("RedPotion", newPos, glm::vec2{64, 64}, 0, 1);
+                        newPotion->AddComponent<SpriteLoader>(true, "sprtShader");
+                        newPotion->AddComponent<SpriteRenderer>(true, "red_potion");
+                        newPotion->AddComponent<Potion>(true);
+                    } else if (ingredientCounts.count("FlowerIngredient")) {
                         ingredientCounts.clear();
                         isEmpty = true;
                         glm::vec2 newPos(GetTransform()->position().x, GetTransform()->position().y - 64);
