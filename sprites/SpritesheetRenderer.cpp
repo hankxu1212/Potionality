7bd689b4968cfc2c2fd11e29e79ff4a51f134cd0
Jesse Liu <jliu644@gmail.com>
2024-11-21 16:51:01 -0500

    Invisibility potion

--- sprites/SpritesheetRenderer.cpp
+++ sprites/SpritesheetRenderer.cpp
@@ -13,22 +13,24 @@ void SpritesheetRenderer::Update()
 {
 	assert(spritesheetLoader);
 
-	float timeDelta = Time::Now - prevTime;
-    if (timeDelta > invFPS)
-    {
-        prevTime = Time::Now;
-        uv_x += 1.0f;
-        if (uv_x >= loop_x) {
-            uv_x = 0;
-            // uv_y += 1.0f;
-            uv_y = loop_y; // TODO: modify this
+    if (isActive) {
+        float timeDelta = Time::Now - prevTime;
+        if (timeDelta > invFPS)
+        {
+            prevTime = Time::Now;
+            uv_x += 1.0f;
+            if (uv_x >= loop_x) {
+                uv_x = 0;
+                // uv_y += 1.0f;
+                uv_y = loop_y; // TODO: modify this
 
-            // if (uv_y >= loop_y)
-                // uv_y = 0;
+                // if (uv_y >= loop_y)
+                    // uv_y = 0;
+            }
         }
-    }
 
-	spritesheetLoader->DrawSprite(spriteToDraw, uv_x, uv_y);
+        spritesheetLoader->DrawSprite(spriteToDraw, uv_x, uv_y);
+    }
 }
 
 void SpritesheetRenderer::SetLoopRegion(float row, float col)
